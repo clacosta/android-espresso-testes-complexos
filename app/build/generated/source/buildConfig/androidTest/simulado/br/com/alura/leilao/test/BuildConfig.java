fdd8b1214a29c8d849b254d9657591f0615c8ab4
Claudio Costa <claudio@imaginaction.com.br>
2019-10-30 18:59:20 -0300

    chore: Merge do branch 01

---
+++ app/build/generated/source/buildConfig/androidTest/simulado/br/com/alura/leilao/test/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.com.alura.leilao.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.com.alura.leilao.test";
+  public static final String BUILD_TYPE = "simulado";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: simulado
+  public static final String URL_BASE = "http://192.168.0.112:8082/";
+}
